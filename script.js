19bdc27a5e07884ae858c20d78c82481f4965dc0
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-11 19:13:48 -0300

    Basic structure

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function start() {
   for (let i = 1; i <= numberUser; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       para.innerHTML = "FizzBuzz";
-    }
+    } else if (i % 3 === 0) {
+      para.innerHTML = "Fizz";
+    } else if (i % 5 === 0) {
+      para.innerHTML = "Buzz";
+    } else para.innerHTML = "zZZzzZZZ";
   }
 }
