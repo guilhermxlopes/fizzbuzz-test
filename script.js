1ab7b97a6f20248482a5525d3c6c257c9f21ab9b
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-12 15:56:14 -0300

    Answer being displayed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function start() {
   var inputUser = window.document.querySelector("input#input");
   var numberUser = Number(inputUser.value);
-  var para = window.document.getElementsByTagName("p")[0];
+  var para = window.document.getElementsByTagName("p")[1];
 
   for (let i = 1; i <= numberUser; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
